1f693fe7d1010a8acb572a499be9d0e19d2e2d7e
Waliullah Kawsar <waliullahx82@gmail.com>
2026-03-26 19:53:00 +0600

    Fix static files - add public folder

--- api/index.js
+++ api/index.js
@@ -1,20 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const rootDir = path.join(__dirname, '..');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(rootDir));
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mindboard';
 
@@ -145,9 +138,9 @@ app.put('/api/users/:username', async (req, res) => {
   }
 });
 
-// Serve index.html for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(rootDir, 'index.html'));
+// Serve index.html for root
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
 });
 
 export default app;
